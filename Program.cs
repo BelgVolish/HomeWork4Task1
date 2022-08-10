8f5636cfa0019535812ab471fc1781fbd6dcc23b
BelgVolish <gleb.shilov.07@bk.ru>
2022-08-10 10:51:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов");
+            int n = int.Parse(Console.ReadLine());
+            int[,] randomarry = new int[m, n];
+            Random r = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                for (int ii = 0; ii < n; ii++)
+                {
+                    randomarry[i, ii] = r.Next(0, 100);
+                    Console.Write(randomarry[i, ii] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
